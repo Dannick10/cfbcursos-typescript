7e7a8b2444ab0039f9f7cca02b1743fd7bb6aa9b
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2024-01-15 18:08:37 -0300

    onions temp e any

---
+++ aula 05/build/aula05.js
@@ -0,0 +1,13 @@
+"use strict";
+let vteste;
+vteste = 'bruno';
+vteste = 10;
+vteste = true;
+let cursos = ['Javascript', 'typescript', 'c++', 500, false];
+let valores = [10, 200, 1000];
+cursos.push('Arduino');
+cursos.push(false);
+valores.push(555);
+console.log(cursos);
+console.log(valores);
+console.log(vteste);
---
+++ aula 05/src/aula05.ts
@@ -0,0 +1,15 @@
+let vteste:string|number|boolean; //onions tempts 
+vteste='bruno';
+vteste=10;
+vteste=true;
+
+let cursos:any[]=['Javascript','typescript','c++',500,false];
+let valores=[10,200,1000];
+
+cursos.push('Arduino');
+cursos.push(false);
+valores.push(555);
+
+console.log(cursos);
+console.log(valores);
+console.log(vteste)
