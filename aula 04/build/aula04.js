e93efe7b25419d3d0ceefeb5709b9846305ae11a
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2024-01-15 17:48:38 -0300

    tipagem typescript

---
+++ aula 04/build/aula04.js
@@ -0,0 +1,7 @@
+"use strict";
+let nome;
+nome = "Daniel";
+let valor;
+valor = 10.5;
+console.log(nome);
+console.log(valor);
---
+++ aula 04/src/aula04.ts
@@ -0,0 +1,8 @@
+let nome:string;
+nome="Daniel";
+
+
+let valor:number; //int, float, bin, hex
+valor=10.5;
+console.log(nome);
+console.log(valor);
