85d106c9d500a1a06df81271e8dbb95e7e7b7d41
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2024-01-18 21:06:37 -0300

    functions

---
+++ aula 11/build/aula10.js
@@ -0,0 +1,15 @@
+"use strict";
+function logar(user, pass) {
+    console.log(user);
+    console.log(pass);
+}
+logar('daniel', 123);
+function soma(n1, n2) {
+    return n1 + n2;
+}
+let res = soma(10, 20);
+console.log(res);
+console.log(typeof (res));
+res.toString();
+console.log(res);
+console.log(typeof (res));
---
+++ aula 11/src/aula10.ts
@@ -0,0 +1,19 @@
+function logar(user:string,pass:number):void{
+    console.log(user)
+    console.log(pass)
+}
+
+
+logar('daniel',123)
+
+function soma(n1:number,n2:number):number{
+    return n1+n2
+}
+
+let res = soma(10,20)
+
+console.log(res)
+console.log(typeof(res))
+res.toString()
+console.log(res)
+console.log(typeof(res))
