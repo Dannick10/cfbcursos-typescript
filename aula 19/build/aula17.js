d0ac71488e968fe5ed76fe9698d799734793668c
Daniel Rocha <102672001+Dannick10@users.noreply.github.com>
2024-01-22 18:09:45 -0300

    super() em typerscript

--- aula 19/build/aula17.js
+++ aula 19/build/aula17.js
@@ -7,19 +7,27 @@ class Conta {
         this.titular = titular;
     }
     getinfo() {
-        console.log(`Numero: ${this.numero} <br> Titular: ${this.titular}`);
+        console.log(`Numero: ${this.numero} \n Titular: ${this.titular} \n cpf|cnpj ${this.cpf || this.cnpj} \n ______________`);
     }
     setTitular(p) {
         return this.titular = p;
     }
 }
 class ContaPf extends Conta {
+    cpf;
+    constructor(cpf, titular) {
+        super(titular);
+        this.cpf = cpf;
+    }
 }
 class ContaPj extends Conta {
+    cnpj;
+    constructor(cnpj, titular) {
+        super(titular);
+        this.cnpj = cnpj;
+    }
 }
-const cont1 = new ContaPf('daniel');
-const cont2 = new ContaPj('Fabricio');
+const cont1 = new ContaPf(222333, 'Daniel');
 cont1.getinfo();
+const cont2 = new ContaPj(15479546, 'Fabricio');
 cont2.getinfo();
-cont1.setTitular('Daniel Rocha');
-cont1.getinfo();
--- aula 19/src/aula17.ts
+++ aula 19/src/aula17.ts
@@ -8,7 +8,8 @@ class Conta{
     }
 
     getinfo():void{
-         console.log(`Numero: ${this.numero} <br> Titular: ${this.titular}`)
+         console.log(`Numero: ${this.numero} \n Titular: ${this.titular} \n cpf|cnpj ${this.cpf || this.cnpj} \n ______________`)
+         
     }
 
     setTitular(p:string):string{
@@ -17,16 +18,26 @@ class Conta{
 }
 
 class ContaPf extends Conta {
+    cpf:Number 
+
+    constructor(cpf:Number,titular:String){
+        super(titular)
+        this.cpf = cpf
+    }
 
 }
 
 class ContaPj extends Conta {
+    cnpj:Number 
 
+    constructor(cnpj:Number,titular:String){
+        super(titular)
+        this.cnpj = cnpj
+    }
 }
 
-const cont1 = new ContaPf('daniel')
-const cont2 = new ContaPj('Fabricio')
+const cont1 = new ContaPf(222333,'Daniel')
 cont1.getinfo()
+
+const cont2 = new ContaPj(15479546,'Fabricio')
 cont2.getinfo()
-cont1.setTitular('Daniel Rocha')
-cont1.getinfo()
